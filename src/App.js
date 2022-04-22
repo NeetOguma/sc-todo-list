2dfe9b3d7a5a5ce107dea808672098f4af19269b
NeetOguma <99900083+NeetOguma@users.noreply.github.com>
2022-04-22 14:17:36 +0900

    add(App): 追加機能　実装

--- src/App.js
+++ src/App.js
@@ -8,18 +8,35 @@ export default function App() {
   //Inputに記入された時用
   const [todo, setTodo] = useState("");
 
+  function handleFormSubmit(e) {
+    e.preventDefault();
+
+    if (todo !== "") {
+      setTodos([
+        ...todos,
+        {
+          id: todos.length + 1,
+          text: todo.trim()
+        }
+      ]);
+    }
+    setTodo("");
+  }
+
   return (
     <>
       <div className="App">
         <form>
           <input name="todo" type="text" placeholder="Create a new Todo!!" />
         </form>
+
         <li>
           {todos.map((todo) => (
             <li>{todo}</li>
           ))}
         </li>
       </div>
+      <p>{todo}</p>
     </>
   );
 }
